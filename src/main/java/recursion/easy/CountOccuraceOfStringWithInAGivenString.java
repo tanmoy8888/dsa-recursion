9154412f19f9657bc3b1bf6fe56f6c6c92180671
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-03-17 12:40:38 +0530

    recursion medium

--- src/main/java/recursion/easy/CountOccuraceOfStringWithInAGivenString.java
+++ src/main/java/recursion/easy/CountOccuraceOfStringWithInAGivenString.java
@@ -4,12 +4,23 @@ package recursion.easy;
  * @author Tanmoy Mukherjee
  */
 public class CountOccuraceOfStringWithInAGivenString {
+    private static final String SOURCE= "hello world, hello universe";
+    private static final String TARGET= "hello";
+
     public static void main(String[] args){
+    System.out.println(
+        "Occurrence is  " + findOccurrenceHelper(SOURCE, TARGET));
+    }
+
+    private static int findOccurrenceHelper(String source,String target){
+      return findOccurrenceHelper(source,target,0);
 
     }
 
     private static int findOccurrenceHelper(String source, String target , int index){
         if(index > source.length()-target.length()) return 0;
-
+        int subProbAns = findOccurrenceHelper(source,target,index+1);
+        boolean found = source.substring(index,index+target.length()).equals(target);
+        return (found) ? subProbAns+ 1 : subProbAns;
     }
 }
---
+++ src/main/java/recursion/medium/SubSetsOfAGivenString.java
@@ -0,0 +1,30 @@
+package recursion.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *  I/P  => [abc]
+ *  O/P  => ["",a,b,c,ab,bc,ca,abc]
+ *
+ * @author Tanmoy Mukherjee
+ */
+public class SubSetsOfAGivenString {
+    private static final String INPUT ="abc";
+    public static void main(String[] args){
+    System.out.println("List of SubSet is :: " + subSets(INPUT));
+    }
+    private  static List<String> subSets(String str){
+    return subSetsHelper(str, 0, "", new ArrayList<String>()).stream().sorted().collect(Collectors.toList());
+    }
+    private static List<String> subSetsHelper(String str, int i , String curr ,List<String> list){
+    if (i == str.length()) {
+        list.add(curr);
+        return list;
+    }
+    subSetsHelper(str,i+1,curr,list);
+    subSetsHelper(str,i+1,curr+str.charAt(i),list);
+    return list;
+    }
+}
