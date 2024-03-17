f785666b39bf519a619cf463fa823575848ee101
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-03-17 13:28:04 +0530

    recursion medium problem

--- src/main/java/recursion/easy/CheckIfArrayIsSorted.java
+++ src/main/java/recursion/easy/CheckIfArrayIsSorted.java
@@ -4,5 +4,26 @@ package recursion.easy;
  * @author Tanmoy Mukherjee
  */
 public class CheckIfArrayIsSorted {
-    
+    private static final int [] ARRAY = {1, 2, 3, 4, 5};
+  //  private static final int [] ARRAY = {1, 2, 4, 3, 5};
+    private static final int ARRAY_SIZE = 5;
+    public static void main(String[] args){
+    System.out.println(
+        "Is given array is sorted :: " + arraySortedOrNot(ARRAY, ARRAY_SIZE));
+    }
+    public static boolean arraySortedOrNot(int []arr, int n){
+        return arraySortedOrNotHelper(arr,n,0);
+    }
+
+    public static boolean arraySortedOrNotHelper(int []arr, int n , int index){
+      if(index == n-1){
+          return true;
+      }
+      if(arr[index] < arr[index+1] || arr[index] == arr[index+1]){
+          return arraySortedOrNotHelper(arr,n,index+1);
+      }
+      else{
+          return false;
+      }
+    }
 }
