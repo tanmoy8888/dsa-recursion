042bc4f0f28721d950cff41634f2026d17f97804
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-04 23:04:30 +0530

    array problems added

---
+++ src/main/java/array/medium/MinimumOperationsMatrix.java
@@ -0,0 +1,45 @@
+package array.medium;
+
+/**
+ * Minimum Operation of Given Type to Make all Elements of a Matrix Equal
+ *
+ * <p>Problem Statement Given an integer K and a matrix of N rows and M columns, the task is to find
+ * the minimum number of operations required to make all the elements of the matrix equal. In a
+ * single operation, K can be added to or subtracted from any element of the matrix.
+ *
+ * <p>Example 1: Input:
+ *
+ * <p>A = [[0, 2, 8],
+ *
+ * <p>[8, 2, 0],
+ *
+ * <p>[0, 2, 8]]
+ *
+ * <p>K = 2
+ *
+ * <p>Output:
+ *
+ * <p>12
+ *
+ * <p>Constraints: 1 <= N, M <= 1000
+ *
+ * <p>1 <= | A | <= 105
+ *
+ * <p>1 <= Ai <= 105
+ *
+ * <p>1 <= k <= 104
+ *
+ * @author Tanmoy Mukherjee
+ */
+public class MinimumOperationsMatrix {
+    public static void main(String[] args){
+
+    }
+
+    public static int minOperations(int n, int m, int k, int A[][]) {
+        //You Can Code Here
+
+
+
+    }
+}
