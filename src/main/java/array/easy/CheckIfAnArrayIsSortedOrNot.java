579c6bdb148884991b8c0a25cafbc611ecd4012c
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-07 14:01:59 +0530

    array problems added

--- src/main/java/array/easy/CheckIfAnArrayIsSortedOrNot.java
+++ src/main/java/array/easy/CheckIfAnArrayIsSortedOrNot.java
@@ -5,16 +5,17 @@ package array.easy;
  */
 public class CheckIfAnArrayIsSortedOrNot {
     public static void main(String[] args){
-        //int [] arr = {1,2,4,5,1,7,1,2};
-        int [] arr = {1,2,4,5,7,9,11};
+        int [] arr = {1,2,4,5,1,7,1,2};
+        //int [] arr = {1,2,2,4,5,7,9,11};
     System.out.println("isSorted(arr) = " + isSorted(arr));
     }
 
     private static boolean isSorted(int [] arr){
-        boolean isSorted = false;
+        boolean isSorted = true;
         for(int i = 0; i < arr.length ; i++) {
-            if(i > 0 && (arr[i] >= arr[i-1])){
-                isSorted =true;
+            if(i > 0 && (arr[i] < arr[i-1])){
+                isSorted =false;
+                break;
             }
         }
         return isSorted;
