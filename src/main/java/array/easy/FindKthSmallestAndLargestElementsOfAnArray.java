6de0e14b94790713449b7a5f323ffcc38dea5155
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-07 15:11:31 +0530

    array problems added

--- src/main/java/array/easy/FindKthSmallestAndLargestElementsOfAnArray.java
+++ src/main/java/array/easy/FindKthSmallestAndLargestElementsOfAnArray.java
@@ -1,6 +1,6 @@
 package array.easy;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Find Kth highest and lowest element of an array
@@ -11,7 +11,7 @@ import java.util.Arrays;
  */
 public class FindKthSmallestAndLargestElementsOfAnArray {
     public static void main(String[] args){
-        int [] arr = {1,2,4,5,7,9,12};
+        int [] arr = {1,2,4,5,1,4,7,9,12,9};
         int K = 2;
         int [] ans = findKthSmallestAndLargestElementsOfAnArray(arr,K);
         System.out.println("Arrays.toString(ans) = " + Arrays.toString(ans));
@@ -19,10 +19,19 @@ public class FindKthSmallestAndLargestElementsOfAnArray {
 
     private static int[] findKthSmallestAndLargestElementsOfAnArray(int [] arr , int K){
         Arrays.sort(arr);
+        int [] dupRemoved = removeDuplicates(arr);
         int [] ans = new int[2];
-        ans[0] = arr[K-1];
-        ans[1] = arr[(arr.length-1)-(K-1)];
+        ans[0] = dupRemoved[K-1];
+        ans[1] = dupRemoved[(dupRemoved.length-1)-(K-1)];
         return ans;
+    }
+
+    private static int[] removeDuplicates(int [] arr){
+    Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < arr.length; i++) {
+                set.add(arr[i]);
+            }
+       return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
