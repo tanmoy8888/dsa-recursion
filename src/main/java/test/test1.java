7bece95b68333da65809babadc7c1a3fc6576685
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-12 02:06:09 +0530

    array problems added

---
+++ src/main/java/test/test1.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Tanmoy Mukherjee
+ */
+public class test1 {
+    public static void main(String[] args){
+
+    }
+    public List<List<Integer>> findPair(int[] arr){
+        List<List<Integer>> finalList= new ArrayList<List<Integer>>();
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[i] + arr[j] == sum){
+                    List<Integer> list = new ArrayList();
+                    list.add(arr[i]);
+                    list.add(arr[j]);
+                }
+            }
+            finalList.add(list);
+        }
+
+    }
+}
