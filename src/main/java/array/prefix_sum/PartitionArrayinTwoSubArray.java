1566ee9b756f9dd4937b871ef64e200df19fadcb
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-14 00:08:52 +0530

    array problems added

--- src/main/java/array/prefix_sum/PartitionArrayinTwoSubArray.java
+++ src/main/java/array/prefix_sum/PartitionArrayinTwoSubArray.java
@@ -6,7 +6,9 @@ package array.prefix_sum;
 public class PartitionArrayinTwoSubArray {
     public static void main(String[] args){
         //int [] arr = {5,3,2,6,3,1};
-        int [] arr = {5,3,5,6,3,1};
+        //int [] arr = {5,3,5,6,3,1};
+        //int [] arr = {4,2,4};
+        int [] arr = {11,37,4,99,16,9,84,23,36,78,6,89,8,81,15,37,34,77,58,11};
     System.out.println("findSubArrayPossible(arr) = " + findSubArrayPossible(arr));
     }
 
--- src/main/java/array/two_pointer_algorithm/PartitionArrayinTwoSubArray.java
+++ src/main/java/array/two_pointer_algorithm/PartitionArrayinTwoSubArray.java
@@ -5,9 +5,13 @@ package array.two_pointer_algorithm;
  */
 public class PartitionArrayinTwoSubArray {
     public static void main(String[] args){
-        int [] arr = {5,3,2,6,3,1};
-        //int [] arr = {1,1,1,1,1,1};
-        //int [] arr = {5,3,5,6,3,1};
+        //int [] arr = {5,3,2,6,3,1};  // true => Correct
+        //int [] arr = {11,37,4,99,16,9,84,23,36,78,6,89,8,81,15,37,34,77,58,11};   // true => Correct
+        //int [] arr = {1,1,1,1,1,1}; // true => Correct
+       // int [] arr = {5,3,5,6,3,1}; //false => Correct
+       // int [] arr = {4,2,4}; // true => Correct
+       // int [] arr = {0,0,0}; // true => Correct
+         int [] arr = {0,0}; // false => Correct
     System.out.println("isSubArrayPossible(arr) = " + isSubArrayPossible(arr));
   }
 
@@ -20,7 +24,7 @@ private static boolean isSubArrayPossible(int [] arr){
     while (start < end){
         prefixSum += arr[start];
         suffixSum += arr[end];
-        if(prefixSum == suffixSum){
+        if( prefixSum == suffixSum){
             return true;
         }
         else{
