cc1ae93b40138bf6002e6e142350c0c68c259058
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-13 21:40:07 +0530

    array problems added

--- src/main/java/array/prefix_sum/PrintSumFromLToRIndex.java
+++ src/main/java/array/prefix_sum/PrintSumFromLToRIndex.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class PrintSumFromLToRIndex {
     public static void main(String[] args){
      int [] arr = {1,2,3,4,5};
-     int l = 1;
+     int l = 0;
      int r = 5;
     System.out.println(
         "Arrays.toString(findSum(arr,l,r)) = " + findSum(arr, l, r));
@@ -16,14 +16,22 @@ public class PrintSumFromLToRIndex {
 
     private static int findSum(int [] arr , int l , int r){
          int [] prefixSumArray = prefixSumArray(arr);
-    System.out.println("Arrays.toString(prefixSumArray) = " + Arrays.toString(prefixSumArray));
-         return prefixSumArray[r]-prefixSumArray[l-1];
+        int left=0;
+         if(l < 1){
+            left =l+1;
+         }
+         int right = r-1;
+
+    System.out.println("prefixSumArray[right] = " + prefixSumArray[right]);
+    System.out.println("prefixSumArray[left - 1] = " + prefixSumArray[left - 1]);
+
+    return prefixSumArray[right] - prefixSumArray[left - 1];
     }
 
     private static int[] prefixSumArray(int [] arr){
         for(int i = 1; i < arr.length; i++) {
           arr[i] += arr[i-1];
-      System.out.println("arr[i] = " + arr[i]); 
+      System.out.println("arr[i] = " + arr[i]);
         }
         return arr;
     }
