4e29a4b081fc6e6b00b2ee011ac41d50fadd4792
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-14 13:52:10 +0530

    array problems added

---
+++ src/main/java/multi_diemtional_array/MatrixAddition.java
@@ -0,0 +1,29 @@
+package multi_diemtional_array;
+
+import java.util.Arrays;
+
+/**
+ * @author Tanmoy Mukherjee
+ */
+public class MatrixAddition {
+    public static void main(String[] args){
+        int [][] A = {{1,2},
+                      {3,4}};
+        int [][] B = {{1,1},
+                      {2,2}};
+        matrixSum(A,B);
+    }
+
+    private static void matrixSum(int [] [] A,int [] [] B ){
+    int row = A.length;
+    int column = A[0].length;
+      int [][] sum = new int[row][column];
+      for(int i = 0; i < row; i++) {
+          for(int j = 0; j < column; j++) {
+              sum[i][j] = A[i][j]+B[i][j];
+        System.out.print(sum[i][j]+" ");
+          }
+          System.out.println();
+      }
+    }
+}
