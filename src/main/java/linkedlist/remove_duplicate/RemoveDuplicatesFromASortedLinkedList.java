2df80b237cd41e61efc29f6ba8abad3e477b5205
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-15 22:59:54 +0530

    array problems added

--- src/main/java/linkedlist/remove_duplicate/RemoveDuplicatesFromASortedLinkedList.java
+++ src/main/java/linkedlist/remove_duplicate/RemoveDuplicatesFromASortedLinkedList.java
@@ -1,19 +1,22 @@
 package linkedlist.remove_duplicate;
 
 /**
+ * Program : Given a sorted linked list , delete all duplicates and return the sorted linked list as well.
+ *
  * @author Tanmoy Mukherjee
  */
 public class RemoveDuplicatesFromASortedLinkedList {
     public static void main(String[] args){
 
         Node<Integer> head= new Node<>(1);
-        Node<Integer> n1 = new  Node<>(2);
-        Node<Integer> n2 = new  Node<>(3);
-        Node<Integer> n3 = new  Node<>(4);
-        Node<Integer> n4 = new  Node<>(5);
-        Node<Integer> n5 = new  Node<>(6);
-        Node<Integer> n6 = new  Node<>(7);
-        Node<Integer> n7 = new  Node<>(8);
+        Node<Integer> n1 = new  Node<>(1);
+        Node<Integer> n2 = new  Node<>(2);
+        Node<Integer> n3 = new  Node<>(3);
+        Node<Integer> n4 = new  Node<>(3);
+        Node<Integer> n5 = new  Node<>(3);
+        Node<Integer> n6 = new  Node<>(4);
+        Node<Integer> n7 = new  Node<>(5);
+
         head.next=n1;
         n1.next = n2;
         n2.next = n3;
@@ -21,19 +24,38 @@ public class RemoveDuplicatesFromASortedLinkedList {
         n4.next = n5;
         n5.next = n6;
         n6.next = n7;
+      traverseLinkedList(head);
+      Node<Integer> newHead = removeDuplicatesFromLinkedList(head);
+      System.out.println("Removed duplicate elemenets.....");
+      traverseLinkedList(newHead);
 
-        Node<Integer> node = findMiddleElement(head);
-        System.out.println("node.val = " + node.val);
-  }
+   }
 
-  private static Node findMiddleElement(Node<Integer> head) {
-        Node<Integer> slow = head;
-        Node<Integer> fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+   private static Node<Integer> removeDuplicatesFromLinkedList(Node<Integer> head){
+        Node<Integer> original = head;
+        Node<Integer> temp = head;
+        Node<Integer> newHead = null;
+        while (original != null){
+              while (original.next != null && original.val == original.next.val){
+                  original = original.next;
+              }
+              if(newHead == null){
+                  newHead = temp = original;
+              }else{
+                  temp.next = original;
+                  temp = original;
+              }
+              original = original.next;
         }
-        return slow;
+        return  newHead;
+   }
+
+    private  static void traverseLinkedList(Node<Integer> head){
+        Node<Integer> temp = head;
+        while (temp != null){
+            System.out.print(temp.val+" -> ");
+            temp = temp.next;
+        }
     }
 
     static class Node<T>{
