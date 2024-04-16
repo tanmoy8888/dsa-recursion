1413db2ad381f1fdfeb2d26166b14092101e20c7
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-16 13:44:34 +0530

    linked list problem added

---
+++ src/main/java/linkedlist/reverse_a_linked_list/ReserveALinkedList.java
@@ -0,0 +1,54 @@
+package linkedlist.reverse_a_linked_list;
+
+
+import linkedlist.practice.medium.DeleteMiddleListNodeOfLinkedList;
+
+import javax.xml.soap.Node;
+
+/**
+ * @author Tanmoy Mukherjee
+ */
+public class ReserveALinkedList {
+    public static void main(String[] args){
+        ListNode head= new ListNode(1);
+        ListNode n1 = new  ListNode(2);
+        ListNode n2 = new  ListNode(3);
+
+        head.next=n1;
+        n1.next = n2;
+        printLinkedList(head);
+        System.out.println("Printing reverse linked list :: ");
+        ListNode newHead = reverseALinkedList(head);
+        printLinkedList(newHead);
+    }
+
+    private static ListNode reverseALinkedList(ListNode head){
+        ListNode previous = head;
+        ListNode current = head.next;
+        head.next = null;
+        while (current != null){
+            ListNode next = current.next;
+            current.next= previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+     }
+
+    private static void printLinkedList(ListNode head){
+        ListNode temp = head;
+        while (temp != null){
+         System.out.print(temp.val +"-->");
+         temp = temp.next;
+         }
+    System.out.println();
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() { val = 0; next = null; }
+        ListNode(int val) { this.val = val; next = null; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
