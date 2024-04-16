156c3ea4fb91a4b921d8963b0b135ef2a2b036d9
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-16 21:18:20 +0530

    linked list problem added

---
+++ src/main/java/linkedlist/single_linkedlist_palindrome/Palindrome.java
@@ -0,0 +1,85 @@
+package linkedlist.single_linkedlist_palindrome;
+
+
+import linkedlist.reverse_a_linked_list.ReserveALinkedList;
+
+/**
+ * @author Tanmoy Mukherjee
+ */
+public class Palindrome {
+    public static void main(String[] args){
+        ListNode head =new ListNode(1);
+        ListNode n1 = new  ListNode(2);
+        ListNode n2 = new  ListNode(3);
+        ListNode n3 = new  ListNode(2);
+        ListNode n4 = new  ListNode(1);
+
+        head.next=n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        printLinkedList(head);
+        // Find middle element of the linked list;
+        ListNode middle = findMiddleNode(head);
+        printLinkedList(middle);
+        // Now reverse the second part
+       boolean isPalindrome = isLinkedListIsPalindrome(head);
+       System.out.println("isPalindrome = " + isPalindrome);
+    }
+
+    private static boolean isLinkedListIsPalindrome(ListNode head){
+        ListNode first = head;
+        ListNode middle = findMiddleNode(first);
+        ListNode second = reverseALinkedList(middle);
+        while (second != null){
+            if(first.val != second.val){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+
+    private static ListNode reverseALinkedList(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode previous = head;
+        ListNode current = head.next;
+        head.next = null;
+        while (current != null){
+            ListNode next = current.next;
+            current.next= previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+    private static ListNode findMiddleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+              slow = slow.next;
+              fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private static void printLinkedList(ListNode head){
+        ListNode temp = head;
+        while (temp != null){
+          System.out.print(temp.val+"-->");
+          temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() { val = 0; next = null; }
+        ListNode(int val) { this.val = val; next = null; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
