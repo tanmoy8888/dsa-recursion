f42785aa0dbbb5b9e2435dfc0c8b8d63324ec3ff
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-18 01:58:49 +0530

    linked list problem added

--- src/main/java/linkedlist/practice/medium/ReverseALinkedList.java
+++ src/main/java/linkedlist/practice/medium/ReverseALinkedList.java
@@ -17,7 +17,9 @@ public class ReverseALinkedList {
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
+
         traverseLinkedList(head);
+
         ListNode newHead = reverseLinkedList(head);
         traverseLinkedList(newHead);
     }
