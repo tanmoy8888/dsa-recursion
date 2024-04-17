7da9b26717a357889d8e98c4d846f40f78dd189a
tanmoy8888 <tanmoy8888@yahoo.co.in>
2024-04-18 01:57:37 +0530

    linked list problem added

--- src/main/java/linkedlist/practice/medium/ReverseALinkedList.java
+++ src/main/java/linkedlist/practice/medium/ReverseALinkedList.java
@@ -18,21 +18,20 @@ public class ReverseALinkedList {
         n2.next = n3;
         n3.next = n4;
         traverseLinkedList(head);
-        System.out.println("After reverse............");
-        ListNode newhead = reverseLinkedList(head);
-        traverseLinkedList(newhead);
+        ListNode newHead = reverseLinkedList(head);
+        traverseLinkedList(newHead);
     }
 
     private static ListNode reverseLinkedList(ListNode head){
         if(head == null || head.next == null) return head;
         ListNode current = head.next;
         ListNode previous = head;
+        // If this is not done then it will go to infinite loop
         head.next = null;
         while (current != null){
             ListNode next = current.next;
             current.next = previous;
-
-            // update
+            // update cuurent snd previous for next iterations
             previous = current;
             current = next;
         }
